1498276ec41af81eeaa304724292b92532cc8c6a
Kashifa Fatima <kashuf236@gmail.com>
2026-01-17 00:47:28 +0530

    Add files via upload

---
+++ 01_variables.c
@@ -0,0 +1,10 @@
+// Program to demonstrate variable declaration, assignment, and output
+#include <stdio.h>
+
+int main() {
+    int a;        // variable declaration
+    a = 6;        // assigning value to variable
+
+    printf("The output of this program is %d", a);  // displaying value
+    return 0;
+}
