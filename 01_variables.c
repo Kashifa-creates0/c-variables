48030a530624f3b5a998bb7e65d830558438eacd
kashifa <kashuf236@gmail.com>
2026-01-17 16:06:13 +0530

    add new file

--- 01_variables.c
+++ 01_variables.c
@@ -3,7 +3,7 @@
 
 int main() {
     int a;        // variable declaration
-    a = 6;        // assigning value to variable
+    a = 6;        // assigning value to variables
 
     printf("The output of this program is %d", a);  // displaying value
     return 0;
